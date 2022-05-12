4abd27b9712ac9dac515453fe71d907baa7fb6f7
jeonghyeonuk <junghunwook456@naver.com>
2022-05-12 12:37:50 +0900

    complete merge

--- app.py
+++ app.py
@@ -31,20 +31,6 @@ from bs4 import BeautifulSoup
 
 SECRET_KEY = 'SPARTA'
 
-
-@app.route('/')
-def home():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
-        return render_template('index.html')
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
-
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
--- static/login.js
+++ static/login.js
@@ -151,3 +151,9 @@ function check_dup() {
         }
     });
 }
+
+function sign_out() {
+    $.removeCookie('mytoken', {path: '/'});
+    alert('로그아웃!')
+    window.location.href = "/"
+}
