6f2f3aee1bde8214a71e8bfaedf9c19f21258ac0
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-05-12 18:19:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,45 +2,39 @@ from pymongo import MongoClient
 import jwt
 import certifi
 import datetime
+import requests
+from bs4 import BeautifulSoup
 import hashlib
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
-import requests
-from bs4 import BeautifulSoup
 
+
+# flask setting
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
-SECRET_KEY = 'SPARTA'
+# mongodb connect
 
 ca = certifi.where()
 
 client = MongoClient('mongodb+srv://test:test@cluster0.mai7p.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
+# 현욱님 DB
+# client2 = MongoClient('mongodb+srv://test:sparta@cluster0.arjwt.mongodb.net/Cluster0?retryWrites=true&w=majority')
+# db2 = client2.dbsparta
 
-@app.route('/')
-def home():
-    countrys = list(db.countrys.find({}, {"_id": False}).limit(10))
-    countrys = list(db.countrys.find({}, {"_id": False}))
-    return render_template("index.html", countrys=countrys)
+# 크롤링
 
+SECRET_KEY = 'SPARTA'
 
 @app.route('/login')
 def login():
-    return render_template('login3.html')
+    msg = request.args.get("msg")
+    return render_template('login3.html', msg=msg)
 
-# token_receive = request.cookies.get('mytoken')
-    # try:
-    #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-    #
-    #     return render_template('index.html')
-    # except jwt.ExpiredSignatureError:
-    #     return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    # except jwt.exceptions.DecodeError:
-    #     return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 @app.route('/user/<username>')
 def user(username):
@@ -67,16 +61,16 @@ def sign_in():
 
     if result is not None:
         payload = {
-            'id': username_receive,
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-            # .decode('utf-8')
 
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
 
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
@@ -102,80 +96,102 @@ def check_dup():
     return jsonify({'result': 'success', 'exists': exists})
 
 
+@app.route('/')
+def main():
+    # countrys = list(db.countrys.find({}, {"_id": False}).limit(10))
+    countrys = list(db.countrys.find({}, {"_id": False}))
+    return render_template("index.html", countrys=countrys)
+
+
 @app.route('/detail/<post_num>')
 def detail(post_num):
-        # post_num에 해당하는 국가 정보 찾음
-        country = db.countrys.find_one({'post_num': int(post_num)})
 
-        # post_num에 해당하는 comment 정보 찾음
-        country_comment = list(db.comments.find({'post_num': int(post_num)}, {'_id': False}))
-        print(country_comment)
+    # post_num에 해당하는 국가 정보 찾음
+    country = db.countrys.find_one({'post_num': int(post_num)})
 
-        comments = list(db.comments.find({}, {'_id': False}))
-        return render_template("detail.html", country_comment=country_comment, country=country)
+    # post_num에 해당하는 comment 정보 찾음
+    country_comment = list(db.comments.find({'post_num': int(post_num)}, {'_id': False}))
+    print(country_comment)
 
-    # 메인화면에서 나라, 이미지, desc 등 정보 크롤링, 저장
+    comments = list(db.comments.find({},{'_id':False}))
+    return render_template("detail.html", country_comment=country_comment, country=country)
+
+
+
+# 메인화면에서 나라, 이미지, desc 등 정보 크롤링, 저장
 @app.route("/info", methods=["POST"])
 def main_info():
-        url = 'https://www.skyscanner.co.kr/news/inspiration/top10-europe-destinations-where-koreans-love-to-go'
+    url = 'https://www.skyscanner.co.kr/news/inspiration/top10-europe-destinations-where-koreans-love-to-go'
 
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-        data = requests.get(url, headers=headers)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url, headers=headers)
 
-        soup = BeautifulSoup(data.text, 'html.parser')
+    soup = BeautifulSoup(data.text, 'html.parser')
 
-        card_image = soup.select('#post-321 > div > div > div.entry-content > figure')
-        card_title = soup.select('#post-321 > div > div > div.entry-content > h2')
+    card_image = soup.select('#post-321 > div > div > div.entry-content > figure')
+    card_title = soup.select('#post-321 > div > div > div.entry-content > h2')
 
-        n = 0
-        desc_list = []
-        short_desc_list = []
-        for desc in soup.select("p", class_="entry-content"):
-            n = n + 1
-            if (n > 2):
-                desc_list.append(desc.text)
-                short_desc_list.append((desc.text).split('.')[0])
+    n = 0
+    desc_list = []
+    short_desc_list = []
+    for desc in soup.select("p", class_="entry-content"):
+        n = n + 1
+        if (n > 2):
+            desc_list.append(desc.text)
+            short_desc_list.append((desc.text).split('.')[0])
 
-        # print(desc_list)
-        print(short_desc_list)
+    # print(desc_list)
+    print(short_desc_list)
 
-        check = list(db.countrys.find({}, {'_id': False}))
-        # print(check)
-        count = 0
+    check = list(db.countrys.find({}, {'_id': False}))
+    # print(check)
+    count = 0
 
-        if len(check) == 0:
-            for image, title in zip(card_image, card_title):
-                count = count + 1
-                image = image.select_one('img')['src']
-                title = title.select_one('a').text
-                # print(image, title)
-                doc = {
-                    'post_num': count,
-                    'title': title,
-                    'image_link': image,
-                    'desc': desc_list[count - 1],
-                    'short_desc': short_desc_list[count - 1]
-                }
-                print(doc)
+    if len(check) == 0:
+        for image, title in zip(card_image, card_title):
+            count = count + 1
+            image = image.select_one('img')['src']
+            title = title.select_one('a').text
+            # print(image, title)
+            doc = {
+                'post_num': count,
+                'title': title,
+                'image_link': image,
+                'desc': desc_list[count - 1],
+                'short_desc': short_desc_list[count - 1]
+            }
+            print(doc)
 
-                db.countrys.insert_one(doc)
+            db.countrys.insert_one(doc)
 
-        return jsonify({'msg': 'main화면정보 저장완료'})
+    return jsonify({'msg': 'main화면정보 저장완료'})
+
+
 
 @app.route('/detail/save_comment', methods=['POST'])
 def save_comment():
-        # (to-do) id 변수로 가져오고 db에 넣어야함
+    # (to-do) id 변수로 가져오고 db에 넣어야함
 
-        # id_receive = request.form["id_give"]
-        comment_receive = request.form["comment_give"]
-        post_num_receive = request.form["post_num_give"]
+    # id_receive = request.form["id_give"]
+    comment_receive = request.form["comment_give"]
+    post_num_receive = request.form["post_num_give"]
 
-        # doc = {"index": index_receive, "id":id_receive, "comment": comment_receive}
-        doc = {"post_num": int(post_num_receive), "comment": comment_receive}
-        db.comments.insert_one(doc)
-        return jsonify({'result': 'success', 'msg': '커멘트 저장'})
+    # doc = {"index": index_receive, "id":id_receive, "comment": comment_receive}
+    doc = {"post_num" : int(post_num_receive) ,"comment": comment_receive}
+    db.comments.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': '커멘트 저장'})
 
+
+@app.route('/')
+def log_out():
+    token_receive = request.cookies.get('mytoken')
+    if token_receive is not None:
+        login_status = 1
+        return render_template('index.html', login=login_status)
+    else:
+        login_status = 0
+        return render_template('index.html', login=login_status)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5500, debug=True)
